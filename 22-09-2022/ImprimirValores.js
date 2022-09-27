e78fe49eb092764cecf4869aba4fd158fe925c56
Henrique Gabriel <75758371+Hgg2020-sudo@users.noreply.github.com>
2022-09-27 10:55:57 -0300

    Mostrando os valores em formato de pirâmide do jeito certo, fora do laço for.

--- 22-09-2022/ImprimirValores.js
+++ 22-09-2022/ImprimirValores.js
@@ -4,7 +4,9 @@ for (let i = 0; i <= n; i++) {
     let s = ''
     for(let j = 1; j <= i; j++){
         s += i + ''
-        console.log(s)
+        /*Mostrando o valor da variavel dentro do laço ele iria repetir os valores em forma de pirâmide e não todos
+        os valores em formato de pirâmide, como o exercicio propoem.
+        */
     }
-
+    console.log(s)
 }
