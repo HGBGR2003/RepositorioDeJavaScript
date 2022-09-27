8f90d1d8b97155f0b84e6ed77f8fa5125310a144
Henrique Gabriel <75758371+Hgg2020-sudo@users.noreply.github.com>
2022-09-27 12:07:18 -0300

    Mostra o ola

--- 22-09-2022/ImprimirValores.js
+++ 22-09-2022/ImprimirValores.js
@@ -7,6 +7,8 @@ for (let i = 0; i <= n; i++) {
         /*Mostrando o valor da variavel dentro do laço ele iria repetir os valores em forma de pirâmide e não todos
         os valores em formato de pirâmide, como o exercicio propoem.
         */
+
+        //Ola
     }
     console.log(s)
 }
