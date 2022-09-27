a85e425df29b83d89dbba15a91c687a55111bfda
Henrique Gabriel <75758371+Hgg2020-sudo@users.noreply.github.com>
2022-09-27 10:30:59 -0300

    Agora colocando os if e else para fazer o Dado rodar, e depois mostar no console.

--- 22-09-2022/ProbabilidadeDODado.js
+++ 22-09-2022/ProbabilidadeDODado.js
@@ -7,3 +7,24 @@ function JogarDado() {
 
 let fDado = [0,0,0,0,0,0]
 
+const count = 10
+
+for (let i = 0; i < count; i++){
+    const n = JogarDado()
+    console.log(n)
+    if (n === 1){
+        fDado[i]++
+    }else if (n === 2) {
+        fDado[i]++
+    }else if (n === 3){
+        fDado[i]++
+    }else if (n === 4){
+        fDado[i]++
+    }else if (n === 5){
+        fDado[i]++
+    }else {
+        fDado[i]++
+    }
+
+}
+
