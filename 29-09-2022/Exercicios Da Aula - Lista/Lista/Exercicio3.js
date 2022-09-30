35c4d3e2036a0748ce6a00caa53e1d5b5d18d6d2
Henrique Gabriel <75758371+Hgg2020-sudo@users.noreply.github.com>
2022-09-30 10:24:45 -0300

    Exercicio Feito. (Não sei se é o correto)

--- 29-09-2022/Exercicios Da Aula - Lista/Lista/Exercicio3.js
+++ 29-09-2022/Exercicios Da Aula - Lista/Lista/Exercicio3.js
@@ -1,3 +1,8 @@
 function calculaImposto(taxa, custo) {
-    
+    let taxa1
+    taxa1 = taxa/100
+    return taxa1 + custo
 }
+//Fiz, só não sei, se é o que o exercicio pediu de verdade.
+let calculo = calculaImposto(50,20)
+console.log(calculo)
