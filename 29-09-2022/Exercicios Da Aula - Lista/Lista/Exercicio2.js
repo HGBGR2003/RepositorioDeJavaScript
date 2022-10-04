cfcfa4f0d4d7668e431de9124d4ddf46172aaf32
Henrique Gabriel <75758371+Hgg2020-sudo@users.noreply.github.com>
2022-10-03 23:13:52 -0300

    Comentário de uma outra forma de fazer o exercicio usando apenas um comando.

--- 29-09-2022/Exercicios Da Aula - Lista/Lista/Exercicio2.js
+++ 29-09-2022/Exercicios Da Aula - Lista/Lista/Exercicio2.js
@@ -1,5 +1,8 @@
 function ValorX(x) {
     if (x > 0) {
+        /*Até o ? é o if,
+        Depois dos dois pontos é else.
+        */
         return console.log("P")
     } else {
         return console.log("N")
