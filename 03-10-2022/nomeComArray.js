4e61ab4be1192fd9e0da4e5d0176ac9189a6e464
Henrique Gabriel <75758371+Hgg2020-sudo@users.noreply.github.com>
2022-10-03 23:15:13 -0300

    Adicionando Nova Lista de Exercicios.

---
+++ 03-10-2022/nomeComArray.js
@@ -0,0 +1,14 @@
+let nomes = []
+let aluras = []
+
+function Gere(){
+    const maxi = 10
+    const mini = 1
+    let gere 
+    gere = Math.floor(Math.random() * (maxi  - mini) + mini)
+    
+    for(let i = 0; i < aluras; i++){
+        aluras[i] = gere
+        console.log(alturas[i])
+    }
+}
