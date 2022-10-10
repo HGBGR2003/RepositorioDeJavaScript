10602b2c4da42556d73c0f8f30507d4ff302f974
Henrique Gabriel <75758371+Hgg2020-sudo@users.noreply.github.com>
2022-10-10 17:52:48 -0300

    Adicionando novos comentários a classe NumeroTruncado1V

--- 06-10-2022/NumeroTruncado1V.js
+++ 06-10-2022/NumeroTruncado1V.js
@@ -2,14 +2,19 @@ let valor
 
 function digiteNumero(){
   
-  //valor = 45.134445555
+  valor = 45.134445555
   //Teste com valor executado só funciona se truncar com número grande.
-  Number(prompt("Informe um número ?"))
+
+
+  /*
+  <-- O valor só vai ser lido se atualizar na variavel de controle.
+  * */
+   //valor = Number(prompt("Informe um número ?"))
 }
 
 digiteNumero()
 
-if (valor != Number()){
+if (Number() !== valor){
   console.log("O seu número é " + valor.toFixed(1) + " esse aqui que mostrou na sua tela!")
 }else{
   
