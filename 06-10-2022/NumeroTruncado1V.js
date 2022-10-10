6f85113ab68b5e52b6f13171e9c24680417d02ad
Henrique Gabriel <75758371+Hgg2020-sudo@users.noreply.github.com>
2022-10-10 17:35:16 -0300

    Classe arrumada com o nome devido

---
+++ 06-10-2022/NumeroTruncado1V.js
@@ -0,0 +1,16 @@
+let valor
+
+function digiteNumero(){
+  
+  //valor = 12.123
+  //Teste com valor executado só funciona se truncar com número grande.
+  Number(prompt("Informe um número ?"))
+}
+
+digiteNumero()
+
+if (valor != Number()){
+  console.log("O seu número é " + valor.toFixed(1) + " esse aqui que mostrou na sua tela!")
+}else{
+  
+}
