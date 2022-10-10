855ce5360fc9d54e10189e073db8c0179330e4ee
Henrique Gabriel <75758371+Hgg2020-sudo@users.noreply.github.com>
2022-10-10 17:29:32 -0300

    O que eu adicionei?

---
+++ 06-10-2022/NumeroVerdade.js
@@ -0,0 +1,14 @@
+let valor
+
+function digiteNumero(){
+  
+  valor = 12.123 //Number(prompt("Informe um número"))
+}
+
+digiteNumero()
+
+if (valor != Number()){
+  console.log(valor.toFixed(1))
+}else{
+  
+}
