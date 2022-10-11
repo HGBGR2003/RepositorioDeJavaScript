7db5aa207fdf47e8281ced1abc30bcdef23608b9
Henrique Gabriel Barbosa Guida Rodrigues <2022108203030033@ifgoiano.local>
2022-10-10 22:26:41 -0300

    Encontrei um jeito, mas não sei se é o correto.

---
+++ 10-10-2022/ConcatenandoString.js
@@ -0,0 +1,12 @@
+function mixUp(letra1 , letra2) {
+    let concatenandoVariavel = letra1 + "" + letra2
+    let final1 = letra1.slice(0,2)
+    final2++
+    let final2 = letra2.slice(0,2)
+    final2++
+    return false
+}
+
+//Tentando Não sei, se vai dar certo
+let principalFuncao = mixUp('mix', 'pod')
+console.log(principalFuncao)
