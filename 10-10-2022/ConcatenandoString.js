5c290f715d5b38c2dc8c714dca65f85c0f5fc2df
Henrique Gabriel Barbosa Guida Rodrigues <2022108203030033@ifgoiano.local>
2022-10-10 22:31:00 -0300

    Atualizado

--- 10-10-2022/ConcatenandoString.js
+++ 10-10-2022/ConcatenandoString.js
@@ -1,10 +1,10 @@
 function mixUp(letra1 , letra2) {
-    let concatenandoVariavel = letra1 + "" + letra2
-    let final1 = letra1.slice(0,2)
+    let concatenandoVariavel = letra1 + " " + letra2
+    let final1 = letra1.slice(2)
+    final1++
+    let final2 = letra2.slice(2)
     final2++
-    let final2 = letra2.slice(0,2)
-    final2++
-    return false
+    return concatenandoVariavel
 }
 
 //Tentando Não sei, se vai dar certo
