9216d1238cc9b41f4f27065455542a71e343252a
Henrique Gabriel Barbosa Guida Rodrigues <2022108203030033@ifgoiano.local>
2022-10-13 22:29:40 -0300

    Adicionado de ultima hora

---
+++ 13-10-2022/VerificandoNumeral.js
@@ -0,0 +1,14 @@
+let vetorNumerico = []
+
+function VerificandoNumero(x1){
+  x1 = Number(prompt("Informe o Número?"))
+  for(var i = 0 ; i < x1; i++){
+    vetorNumerico = vetorNumerico[i]
+    console.log(vetorNumerico)
+  }
+  
+}
+
+
+
+VerificandoNumero()
