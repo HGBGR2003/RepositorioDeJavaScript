edbe1720c5a33c3cd597db41670b4f4b7d5e6ef7
Henrique Gabriel <75758371+Hgg2020-sudo@users.noreply.github.com>
2022-10-25 09:33:32 -0300

    Pasta Atualizada

---
+++ 03-10-2022/ReverterNomes.js
@@ -0,0 +1,8 @@
+let nomes = []
+let controleDoNome = 2
+
+for (let i = 0; i < controleDoNome ; i++) {
+    nomes[i] = prompt("Digite um nome ?")
+}
+
+console.log(nomes.reverse())
--- 13-10-2022/VerificandoOSValoresParaSerNumero.js
+++ 13-10-2022/VerificandoOSValoresParaSerNumero.js
@@ -1,14 +1,22 @@
 let vetorNumerico = []
+const controledoNumero = 2
 
-function VerificandoNumero(x1){
-  x1 = Number(prompt("Informe o Número?"))
-  for(var i = 0 ; i < x1; i++){
-    vetorNumerico = vetorNumerico[i]
-    console.log(vetorNumerico)
+  for(let i = 0 ; i < controledoNumero; i++){
+    let num =  prompt("Informe um Número?")
+    num = Number(num)
+    if (isNaN(num)){
+      console.log("Código Inválido")
+    }else{
+      vetorNumerico.push(num) //<= O vetor irá colocar o valor da variável num dentro do vetor principal.
+    }
   }
+
+let resultado = vetorNumerico.sort(function (a, b){return a-b})
+console.log(resultado)
+console.log(resultado.reverse())
+
   
-}
 
 
 
-VerificandoNumero()
+
