6fc91f6ea7f19d586fcfd1ace9b3caee843b6b58
Henrique Gabriel <75758371+Hgg2020-sudo@users.noreply.github.com>
2022-11-04 21:22:07 -0300

    Exercicio Feito mais com um problema.

--- 31-10-2022/DiasEmMes.js
+++ 31-10-2022/DiasEmMes.js
@@ -1,12 +1,13 @@
-let quantidadeDias = Number(prompt("Informe o mês?"))
+let mesDigitado = prompt("Informe o mês")
+function MostrarData(ano1) {
+    let dias = [30,31]
+   let ano = ano1
+    let data = new Date(ano,mesDigitado-1,0)
+    if (mesDigitado == 1){
+        return ( (ano % 4 === 0) && ( (ano % 100 !== 0 ) || (ano % 400 === 0) ) ) ? 29 : 28
+    } else {
+        return dias [(mesDigitado + (mesDigitado < 7 ? 1:0)) % 2]
+    }
+}
 
-function validarDados(){
-     if (quantidadeDias >= 0 && quantidadeDias < 11){
-         console.log("Foi")
-     }else {
-         console.log("Não Foi")
-     }
-
- }
-
-validarDados()
+console.log(MostrarData(2022))
